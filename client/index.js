83d1dd655efe31c7589a384430dffc8177038e63
Deep Prajapati <dprajap2@gmail.com>
2021-11-30 19:07:23 -0500

    Feat: Add seed file and seed script

--- client/index.js
+++ client/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store'
-import '../'
+import '../public/style.css'
 
 ReactDOM.render(
   <Provider store={store}>
-  <div>Hello, world!</div>,
+  <div>Hello, world!</div>
   </Provider>,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
 );
